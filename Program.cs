cf56f672b5809a2c7fcac64502741a2dce47f81d
lucassouza4 <lussilva1998@gmail.com>
2024-12-02 19:29:24 -0300

    feat: adicionado classe Posicao

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using tabuleiro;
+
+namespace xadrez{
+    class Program{
+        public static void Main(){
+            Posicao p = new(3,4);
+            Console.WriteLine(p);
+        }
+    }
+}
